c62d7bd6f6768cf104611b08d41e5830056bae6f
goodbedford <bedford@goodbedford.com>
2015-06-30 16:49:31 -0700

    added underscore and add new items to list functionality

--- scripts.js
+++ scripts.js
@@ -8,64 +8,17 @@ $body = $("body");
   var $listParent = $("#list");
   var $formBox = $("#formBox");
   var $dateInput = $("#myDate");
-  var listOfTask = [
-        {title: "clean room",
-         task: "hang clothes, sweep floor, water plants",
-         date: new Date(2023, 6, 7)},
-        {title: "task2",
-         task: "hang clothes, sweep floor, water plants",
-         date: new Date(2015, 6, 7)},
-        {title: "task3",
-          task: "hang clothes, sweep floor, water plants",
-          date: new Date(2015, 9, 13)},
-        {title: "task4",
-          task: "hang something something plants",
-          date: new Date(2015, 9, 7)}
-  ];
+  var listTemplate = _.template( $("#list-template").html() );
 
-  var displayTasks = function(listOfTask, parentUl){
-    $.each(listOfTask, function(index, item){
-      var $tempLi = $("<li />");
-      var tempItem = "<span class='miniHeader'>Title: " + item.title + "</span>";
-          tempItem += "<br />";
-          tempItem += "Task: " + item.task;
-          tempItem += "<br />";
-          tempItem += "<span id='dateText' class='date-display'>Due Date: " + item.date.toLocaleDateString("en-US") + "</span>";
-        $tempLi.html( tempItem );
-        parentUl.prepend($tempLi);
-    });
-  }
-  // var addToList = function(evt,liParent, item){
-  //   evt.preventDefault();
-  //   console.log("I was clicked");
-  //   var $listItem = $("<li></li>");
-  //   $listItem.val( item.val() );
-  //   liParent.append($listItem);
-  // }
-  
-  var addToList = function(){
-    event.preventDefault();
-    console.log("I was clicked");
-    if($newItem.val() != "" && 
-       $newItemTitle.val() != "" &&
-       $dateInput.val() != ""){ 
-      var $listItem = $("<li></li>");
-      var tempTask = "<span class='miniHeader'>Title: " + $newItemTitle.val() + "</span>";
-          tempTask += "<br />";
-          tempTask += "Task: " + $newItem.val();
-          tempTask += "<br />";
-          tempTask += "<span id='dateText' class='date-display'>Due Date: " + dateFormater($dateInput.val() )  + "</span>";
-      $listItem.html( tempTask );
-      $listParent.prepend($listItem);
-      $newItemTitle.val("");
-      $newItem.val("");
-      $dateInput.val("");
-    }
-  }
+  var ToDo = function (title, task, date){
+    this.title = title,
+    this.task = task,
+    this.date = date;
+  };
   var dateFormater = function(dTxt){
     // 2023-08-23
     // new Date(2015, 9, 13)
-    console.log("this is dTxt: " + dTxt);
+    //console.log("this is dTxt: " + dTxt);
     var dateArr = dTxt.split("-");
     var tempDate= { month: dateArr[1],
             day: dateArr[2],
@@ -76,6 +29,102 @@ $body = $("body");
 
     return dStr;
   }
+ 
+ //data model
+ var task1 = new ToDo()
+
+
+ var listOfTasks = [
+       {title: "clean room",
+        task: "hang clothes, sweep floor, water plants",
+        date: new Date(2023, 6, 7)},
+       {title: "task2",
+        task: "hang clothes, sweep floor, water plants",
+        date: new Date(2015, 6, 7)},
+       {title: "task3",
+         task: "hang clothes, sweep floor, water plants",
+         date: new Date(2015, 9, 13)},
+       {title: "task4",
+         task: "hang something something plants",
+         date: new Date(2015, 9, 7)}
+ ];
+
+ for(var i =0; i < listOfTasks.length; i++){
+    console.log(" inside for loop" + listOfTasks[i].date.toLocaleDateString("en-US"));  
+    listOfTasks[i].date = listOfTasks[i].date.toLocaleDateString("en-US");
+    //console.log(listOfTasks[i].date)
+ }
+  _.each(listOfTasks, function(task, index){
+    var $task =  $(listTemplate(task) );
+        //$task.attr("data-index", index);
+        $listParent.prepend($task);
+  });
+
+  var displayTasks = function(listOfTasks, parentUl){
+    $.each(listOfTasks, function(index, item){
+      var $tempLi = $("<li />");
+      var tempItem = "<span class='miniHeader'>Title: " + item.title + "</span>";
+          tempItem += "<br />";
+          tempItem += "Task: " + item.task;
+          tempItem += "<br />";
+          tempItem += "<span id='dateText' class='date-display'>Due Date: " + item.date.toLocaleDateString("en-US") + "</span>";
+        $tempLi.html( tempItem );
+        parentUl.prepend($tempLi);
+    });
+  }
+  var addToList = function(){
+    event.preventDefault();
+    console.log("I was clicked");
+    var tempTodo = new ToDo($newItemTitle.val(), $newItemTitle.val(), dateFormater($dateInput.val() ) );
+    listOfTasks.push(tempTodo);
+    console.log(listOfTasks)
+    //console.log(tempTodo.title, tempTodo.task, tempTodo.date);
+    $task = $(listTemplate(tempTodo) );
+    $listParent.prepend($task);
+    // if($newItem.val() != "" && 
+    //    $newItemTitle.val() != "" &&
+    //    $dateInput.val() != ""){ 
+    //   var $listItem = $("<li></li>");
+    //   var tempTask = "<span class='miniHeader'>Title: " + $newItemTitle.val() + "</span>";
+    //       tempTask += "<br />";
+    //       tempTask += "Task: " + $newItem.val();
+    //       tempTask += "<br />";
+    //       tempTask += "<span id='dateText' class='date-display'>Due Date: " + dateFormater($dateInput.val() )  + "</span>";
+    //   $listItem.html( tempTask );
+    //   $listParent.prepend($listItem);
+    //   $newItemTitle.val("");
+    //   $newItem.val("");
+    //   $dateInput.val("");
+    //}
+  }
+  // var addToList = function(evt,liParent, item){
+  //   evt.preventDefault();
+  //   console.log("I was clicked");
+  //   var $listItem = $("<li></li>");
+  //   $listItem.val( item.val() );
+  //   liParent.append($listItem);
+  // }
+  
+  // var addToList = function(){
+  //   event.preventDefault();
+  //   console.log("I was clicked");
+  //   if($newItem.val() != "" && 
+  //      $newItemTitle.val() != "" &&
+  //      $dateInput.val() != ""){ 
+  //     var $listItem = $("<li></li>");
+  //     var tempTask = "<span class='miniHeader'>Title: " + $newItemTitle.val() + "</span>";
+  //         tempTask += "<br />";
+  //         tempTask += "Task: " + $newItem.val();
+  //         tempTask += "<br />";
+  //         tempTask += "<span id='dateText' class='date-display'>Due Date: " + dateFormater($dateInput.val() )  + "</span>";
+  //     $listItem.html( tempTask );
+  //     $listParent.prepend($listItem);
+  //     $newItemTitle.val("");
+  //     $newItem.val("");
+  //     $dateInput.val("");
+  //   }
+  // }
+
   var d1 = new Date();
   var d2 = new Date(2015, 6, 30);
   var one_day = 1000 * 60 * 60 * 24;
@@ -85,7 +134,7 @@ $body = $("body");
       var daysDiff = dateDifferenceMs / one_day;
       return Math.round(daysDiff);
     }
-  console.log(dateDiff(d1,d2));
+  //console.log(dateDiff(d1,d2));
 
   var taskDueAlert = function(d1,d2, dFunc){
     var daysToAlert = dFunc(d1,d2);
@@ -109,7 +158,7 @@ $body = $("body");
   $formBox.on("submit", addToList);
 
   $listParent.on("click", "li", removeListItem);
-  displayTasks(listOfTask, $listParent);
+  //displayTasks(listOfTask, $listParent);
     
 });
 
